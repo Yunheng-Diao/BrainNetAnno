b87c8d3d8a3fc29d7fff944cc97e8dcfdcfce377
Yunheng-Diao <diaoyunheng@163.com>
2025-12-08 19:37:04 +0800

    Build API documentation

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'BrainNetAnno'
+copyright = '2025, BrainNetAnno'
+author = 'BrainNetAnno contributors'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # for Google/NumPy docstrings
+    'sphinx.ext.viewcode',
+]
+
+# If you want to generate API docs automatically, you can enable sphinx.ext.autosummary
+# extensions.append('sphinx.ext.autosummary')
+# autosummary_generate = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Autodoc settings
+autodoc_member_order = 'bysource'
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
